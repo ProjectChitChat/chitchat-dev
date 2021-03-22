9acc27891dc519a269b51caf78260b4f29ed154e
Dabossmanov <ovie.aloko@outlook.com>
2021-03-22 20:00:31 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react"
+import Signup from "./loginComponents/Signup"
+import { Container } from "react-bootstrap"
+import { AuthProvider } from "./contexts/AuthContext"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Dashboard from "./Dashboard"
+import Login from "./loginComponents/Login"
+import PrivateRoute from "./loginComponents/PrivateRoute"
+import ForgotPassword from "./loginComponents/ForgotPassword"
+import UpdateProfile from "./loginComponents/UpdateProfile"
+
+function App() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  )
+}
+
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0OPbWHhYDQkeLBXucLXcQoVCBwVHIsrE",
+  authDomain: "chat-app-da935.firebaseapp.com",
+  projectId: "chat-app-da935",
+  storageBucket: "chat-app-da935.appspot.com",
+  messagingSenderId: "165611957165",
+  appId: "1:165611957165:web:707f8f893bd49980150f24",
+  measurementId: "G-92EWVRGTPH"
+};
+
+export const auth = app.auth()
+export default app
