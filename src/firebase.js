4ba080199b1a897b09655ce76fe086f375759563
Dabossmanov <ovie.aloko@outlook.com>
2021-03-23 10:31:05 +0000

    Yes

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
-import firebase from 'firebase/app'
+import firebase from "firebase/app"
 import "firebase/auth"
 
-const firebaseConfig = {
+const app = firebase.initializeApp({
   apiKey: "AIzaSyA0OPbWHhYDQkeLBXucLXcQoVCBwVHIsrE",
   authDomain: "chat-app-da935.firebaseapp.com",
   projectId: "chat-app-da935",
@@ -9,7 +9,7 @@ const firebaseConfig = {
   messagingSenderId: "165611957165",
   appId: "1:165611957165:web:707f8f893bd49980150f24",
   measurementId: "G-92EWVRGTPH"
-};
+})
 
 export const auth = app.auth()
 export default app
